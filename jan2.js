df3f3ef952d70e2f0cb13789b95f32f2bc2371fb
sofiamatulis <sosomatulis@hotmail.com>
2018-01-02 17:03:02 -0500

    Using for each method instead of the previous loop

--- jan2.js
+++ jan2.js
@@ -1,3 +1,20 @@
+//
+// You're standing in a room with "digitization quarantine" written in LEDs along one wall. The only door is locked, but it includes a small interface. "Restricted Area - Strictly No Digitized Users Allowed."
+//
+// It goes on to explain that you may only leave by solving a captcha to prove you're not a human. Apparently, you only get one millisecond to solve the captcha: too fast for a normal human, but it feels like hours to you.
+//
+// The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
+//
+// For example:
+//
+// 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
+// 1111 produces 4 because each digit (all 1) matches the next.
+// 1234 produces 0 because no digit matches the next.
+// 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
+// What is the solution to your captcha?
+
+
+
 // 1- create a function that takes a number
 // 2- create an empty array
 // 4- Create a loop to compare the 1st number to the one next to it and so on. and then the last one with the first one
@@ -12,40 +29,29 @@ function addNumber(number) {
   var sum = 0;
   // number is being converted to a string
   number = number.toString();
-  // looping through the string
+  // looping through the array
   for (i = 0; i < number.length; i++) {
-    // converting the string to a number
-    // number = parseFloat(number);
-    // digits.push(+number.charAt(i));
     // adding it to the array
     digits.push(number[i]);
-
   }
-  // looping through the string and comparing it
-  for (i = 0; i < digits.length; i++) {
-    if (digits[i] === digits[i+1]) {
-      results.push(digits[i]);
+
+  digits.forEach(function(element, index){
+    if (element === digits[index+1]) {
+      results.push(element);
     }
-  }
+  })
 
+  // comparing the first one to the last one
   if (digits[0] === digits[digits.length - 1 ]) {
     results.push(digits[0]);
   }
 
-  for (i=0; i < results.length; i++){
-    sum += parseInt(results[i]);
-  }
+  results.forEach(function(element){
+    sum += parseInt(element);
+  })
   console.log(sum);
 
 }
 
-addNumber(11);
 
-// There are 3 JavaScript methods that can be used to convert variables to numbers:
-//
-// The Number() method
-// The parseInt() method
-// The parseFloat() method
-
-
-// /The charAt() method returns the character at the specified index in a string.
+addNumber(312213);
